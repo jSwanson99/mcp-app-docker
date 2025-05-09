552ac4e8cdc6521b0656839f439e60bb7fac76a5
Jonathan Swanson <jonathan.swanson.dev@gmail.com>
2025-05-08 21:57:16 -0400

    verify pyrepl working in mcp client

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .PHONY: start stop restart build
 
 mcp-server-pyrepl := "localhost/mcp-server-pyrepl"
+mcp-filesystem:= "localhost/mcp-filesystem"
 mcp-app := "localhost/mcp-app"
 
 restart: stop start
@@ -13,4 +14,5 @@ start: build
 
 build:
 	docker build -f ./mcp-servers/py-repl/Dockerfile -t $(mcp-server-pyrepl) ./mcp-servers/py-repl
+	docker build -f ./mcp-servers/mcp-filesystem/Dockerfile -t $(mcp-filesystem) ./mcp-servers/mcp-filesystem
 	docker build -f ./mcp-client/Dockerfile -t $(mcp-app) ./mcp-client
--- mcp-client/main.py
+++ mcp-client/main.py
@@ -9,7 +9,17 @@ fast = FastAgent("fast-agent agent_one (mcp server)")
 # Define the agent
 @fast.agent(
     name="agent_one",
-    instruction="You are a helpful AI Agent.",
+    instruction="""
+You have access to a Python 3.12 interpreter and you can use this to analyse
+and process data. Common analysis packages such as Pandas, Seaborn and
+Matplotlib are already installed. You can add further packages if needed.
+
+Don't forget to import something before using it
+
+Data files are accessible from the /mnt/data/ directory
+(this is the current working directory).
+Visualisations should be saved as .png files in the current working directory.
+    """,
     servers=["pyrepl"]
 )
 async def main():
